1d99e458dd70c1c617604b503843b07eb7ea7978
damianamalraj <damian.amalraj@hotmail.com>
2022-06-02 23:33:32 +0200

    Work with service worker cashe and stuff

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,12 +1,29 @@
 /* eslint-disable no-restricted-globals */
 console.log("Hello from Service Worker!!");
 const statics = self.__WB_MANIFEST;
+console.log("kmflkmlkfmv" + statics);
 
 self.addEventListener("install", (event) => {
   console.log("Installing!");
+  caches.open("v1").then((cache) => {
+    cache.add("../build/index.html");
+  });
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   console.log("Activate!");
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // If it is in in cache, respond with it
+      if (response) {
+        return response;
+      }
+      // Otherwise, go to network
+      return fetch(event.request);
+    })
+  );
 });
